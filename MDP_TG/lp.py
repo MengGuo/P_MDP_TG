9da1b0ec90f2d4b4a449a4e87d180982b139d216
MengGuo <guomeng3@gmail.com>
2017-06-08 21:12:08 +0200

    add combined policy

--- MDP_TG/lp.py
+++ MDP_TG/lp.py
@@ -8,6 +8,86 @@ from collections import defaultdict
 import random
 
 
+def syn_full_plan_comb(prod_mdp, gamma, alpha=1):
+    #----Optimal plan synthesis, total cost over plan prefix and suffix----
+    print "==========[Optimal full plan synthesis start]=========="
+    Plan = []
+    for l, S_fi in enumerate(prod_mdp.Sf):
+        print "---for one S_fi---"
+        plan = []
+        for k, MEC in enumerate(S_fi):
+            plan_prefix, prefix_cost, prefix_risk, y_in_sf, Sr, Sd = syn_plan_prefix(prod_mdp, MEC, gamma)
+            print "Best plan prefix obtained, cost: %s, risk %s" %(str(prefix_cost), str(prefix_risk))
+            if y_in_sf:
+                plan_suffix, suffix_cost, suffix_risk = syn_plan_suffix(prod_mdp, MEC, y_in_sf)
+                print "Best plan suffix obtained, cost: %s, risk %s" %(str(suffix_cost), str(suffix_risk))
+            else:
+                plan_suffix = None
+            if plan_prefix  and plan_suffix:
+                plan.append([[plan_prefix, prefix_cost, prefix_risk, y_in_sf], [plan_suffix, suffix_cost, suffix_risk], [MEC[0], MEC[1], Sr, Sd]])
+        if plan:
+            best_k_plan = min(plan, key=lambda p: p[0][1] + alpha*p[1][1])
+            Plan.append(best_k_plan)
+        else: 
+            "No valid found!"
+    if Plan:
+        print "========================="
+        print " || Final compilation  ||"
+        print "========================="
+        best_all_plan = min(Plan, key=lambda p: p[0][1] + alpha*p[1][1])
+        print 'Best plan prefix obtained for %s states in Sr' %str(len(best_all_plan[0][0]))
+        print 'cost: %s; risk: %s '%(best_all_plan[0][1], best_all_plan[0][2])
+        print 'Best plan suffix obtained for %s states in Sf' %str(len(best_all_plan[1][0]))
+        print 'cost: %s; risk: %s '%(best_all_plan[1][1], best_all_plan[1][2])
+        plan_bad = syn_plan_bad(prod_mdp, best_all_plan[2])
+        print 'Plan for bad states obtained for %s states in Sd' %str(len(best_all_plan[2][3]))
+        best_all_plan.append(plan_bad)
+        return best_all_plan
+    else:
+        print "No valid plan found"
+        return None
+
+
+def syn_full_plan_rex_comb(prod_mdp, gamma, d, alpha=1):
+    #----Relaxed optimal plan synthesis, total cost over plan prefix and suffix----    
+    print "==========[Relaxed full plan synthesis start]"
+    Plan = []
+    for l, S_fi in enumerate(prod_mdp.Sf):
+        print "---for one S_fi---"
+        plan = []
+        for k, MEC in enumerate(S_fi):
+            plan_prefix, prefix_cost, prefix_risk, y_in_sf, Sr, Sd = syn_plan_prefix(prod_mdp, MEC, gamma)
+            print "Best plan prefix obtained, cost: %s, risk %s" %(str(prefix_cost), str(prefix_risk))
+            if y_in_sf:
+                plan_suffix, suffix_cost, suffix_risk = syn_plan_suffix_rex(prod_mdp, MEC, d, y_in_sf)
+                print "Best plan suffix obtained, cost: %s, risk %s" %(str(suffix_cost), str(suffix_risk))
+            else:
+                plan_suffix = None
+            if plan_prefix  and plan_suffix:
+                plan.append([[plan_prefix, prefix_cost, prefix_risk, y_in_sf], [plan_suffix, suffix_cost, suffix_risk], [MEC[0], MEC[1], Sr, Sd]])
+        if plan:
+            best_k_plan = min(plan, key=lambda p: p[0][1] + alpha*p[1][1])
+            Plan.append(best_k_plan)
+        else: 
+            "No valid found!"
+    if Plan:
+        print "========================="
+        print " || Final compilation  ||"
+        print "========================="
+        best_all_plan = min(Plan, key=lambda p: p[0][1] + alpha*p[1][1])
+        print 'Best plan prefix obtained for %s states in Sr' %str(len(best_all_plan[0][0]))
+        print 'cost: %s; risk: %s '%(best_all_plan[0][1], best_all_plan[0][2])
+        print 'Best plan suffix obtained for %s states in Sf' %str(len(best_all_plan[1][0]))
+        print 'cost: %s; risk: %s '%(best_all_plan[1][1], best_all_plan[1][2])
+        plan_bad = syn_plan_bad(prod_mdp, best_all_plan[2])
+        print 'Plan for bad states obtained for %s states in Sd' %str(len(best_all_plan[2][3]))
+        best_all_plan.append(plan_bad)
+        return best_all_plan
+    else:
+        print "No valid plan found"
+        return None
+    
+
 def syn_full_plan(prod_mdp, gamma, alpha=1):
     #----Optimal plan synthesis, total cost over plan prefix and suffix----
     print "==========[Optimal full plan synthesis start]=========="
