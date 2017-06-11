f9b742ff304d88915a649068a92e27dd718df605
MengGuo <guomeng3@gmail.com>
2017-06-11 21:38:03 +0200

    add combined lp

--- MDP_TG/lp.py
+++ MDP_TG/lp.py
@@ -14,73 +14,19 @@ def syn_full_plan_comb(prod_mdp, gamma, alpha=1):
     Plan = []
     for l, S_fi in enumerate(prod_mdp.Sf):
         print "---for one S_fi---"
-        plan = []
-        for k, MEC in enumerate(S_fi):
-            plan_prefix, prefix_cost, prefix_risk, y_in_sf, Sr, Sd = syn_plan_prefix(prod_mdp, MEC, gamma)
-            print "Best plan prefix obtained, cost: %s, risk %s" %(str(prefix_cost), str(prefix_risk))
-            if y_in_sf:
-                plan_suffix, suffix_cost, suffix_risk = syn_plan_suffix(prod_mdp, MEC, y_in_sf)
-                print "Best plan suffix obtained, cost: %s, risk %s" %(str(suffix_cost), str(suffix_risk))
-            else:
-                plan_suffix = None
-            if plan_prefix  and plan_suffix:
-                plan.append([[plan_prefix, prefix_cost, prefix_risk, y_in_sf], [plan_suffix, suffix_cost, suffix_risk], [MEC[0], MEC[1], Sr, Sd]])
-        if plan:
-            best_k_plan = min(plan, key=lambda p: p[0][1] + alpha*p[1][1])
-            Plan.append(best_k_plan)
+        plan_fi = syn_plan_comb(prod_mdp, S_fi, gamma, alpha)
+        if plan_fi:
+            Plan.append(plan_fi)
         else: 
-            "No valid found!"
+            "No valid found in S_fi!"
     if Plan:
         print "========================="
         print " || Final compilation  ||"
         print "========================="
-        best_all_plan = min(Plan, key=lambda p: p[0][1] + alpha*p[1][1])
-        print 'Best plan prefix obtained for %s states in Sr' %str(len(best_all_plan[0][0]))
-        print 'cost: %s; risk: %s '%(best_all_plan[0][1], best_all_plan[0][2])
-        print 'Best plan suffix obtained for %s states in Sf' %str(len(best_all_plan[1][0]))
-        print 'cost: %s; risk: %s '%(best_all_plan[1][1], best_all_plan[1][2])
-        plan_bad = syn_plan_bad(prod_mdp, best_all_plan[2])
-        print 'Plan for bad states obtained for %s states in Sd' %str(len(best_all_plan[2][3]))
-        best_all_plan.append(plan_bad)
-        return best_all_plan
-    else:
-        print "No valid plan found"
-        return None
-
-
-def syn_full_plan_rex_comb(prod_mdp, gamma, d, alpha=1):
-    #----Relaxed optimal plan synthesis, total cost over plan prefix and suffix----    
-    print "==========[Relaxed full plan synthesis start]"
-    Plan = []
-    for l, S_fi in enumerate(prod_mdp.Sf):
-        print "---for one S_fi---"
-        plan = []
-        for k, MEC in enumerate(S_fi):
-            plan_prefix, prefix_cost, prefix_risk, y_in_sf, Sr, Sd = syn_plan_prefix(prod_mdp, MEC, gamma)
-            print "Best plan prefix obtained, cost: %s, risk %s" %(str(prefix_cost), str(prefix_risk))
-            if y_in_sf:
-                plan_suffix, suffix_cost, suffix_risk = syn_plan_suffix_rex(prod_mdp, MEC, d, y_in_sf)
-                print "Best plan suffix obtained, cost: %s, risk %s" %(str(suffix_cost), str(suffix_risk))
-            else:
-                plan_suffix = None
-            if plan_prefix  and plan_suffix:
-                plan.append([[plan_prefix, prefix_cost, prefix_risk, y_in_sf], [plan_suffix, suffix_cost, suffix_risk], [MEC[0], MEC[1], Sr, Sd]])
-        if plan:
-            best_k_plan = min(plan, key=lambda p: p[0][1] + alpha*p[1][1])
-            Plan.append(best_k_plan)
-        else: 
-            "No valid found!"
-    if Plan:
-        print "========================="
-        print " || Final compilation  ||"
-        print "========================="
-        best_all_plan = min(Plan, key=lambda p: p[0][1] + alpha*p[1][1])
-        print 'Best plan prefix obtained for %s states in Sr' %str(len(best_all_plan[0][0]))
-        print 'cost: %s; risk: %s '%(best_all_plan[0][1], best_all_plan[0][2])
-        print 'Best plan suffix obtained for %s states in Sf' %str(len(best_all_plan[1][0]))
-        print 'cost: %s; risk: %s '%(best_all_plan[1][1], best_all_plan[1][2])
-        plan_bad = syn_plan_bad(prod_mdp, best_all_plan[2])
-        print 'Plan for bad states obtained for %s states in Sd' %str(len(best_all_plan[2][3]))
+        best_all_plan = min(Plan, key=lambda p: p[0])
+        print 'Total cost: %s, prefix cost:%s, prefix risk: %s, suffix cost:%s, suffix risk:%s'%(best_all_plan[0], best_all_plan[2],best_all_plan[3], best_all_plan[5], best_all_plan[6])
+        plan_bad = syn_plan_bad(prod_mdp, best_all_plan[7])
+        print 'Plan for bad states obtained for %s states in Sd' %str(len(best_all_plan[7][3]))
         best_all_plan.append(plan_bad)
         return best_all_plan
     else:
@@ -775,5 +721,295 @@ def rd_act_by_plan(prod_mdp, best_plan, prod_state, I):
         #print 'action chosen: %s' %str(U[k])
         return U[k], 2, I
 
-    
-        
+            
+def syn_plan_comb(prod_mdp, S_fi, gamma, alpha):
+    #----Synthesize combined optimal plan prefix and suffix
+    #----with bounded risk and minimal mean total cost----
+    print "===========[plan prefix synthesis starts]==========="
+    Sf = set()
+    for mec in S_fi:
+        Sf.union(mec[0]) # mec = (sf, ip, act)
+    delta = 1.0
+    for init_node in prod_mdp.graph['initial']:
+        path_init = single_source_shortest_path(prod_mdp, init_node)
+        print 'Reachable from init size: %s' %len(path_init.keys())
+        if not set(path_init.keys()).intersection(Sf):
+            print "Initial node can not reach Sf"
+            return None, None, None, None, None, None
+        Sn = set(path_init.keys()).difference(Sf)
+        #----find bad states that can not reach MEC
+        simple_digraph = DiGraph()
+        simple_digraph.add_edges_from(((v,u) for u,v in prod_mdp.edges()))
+        reachable_set = set()
+        for mec in S_fi:
+            ip = mec[1]
+            path = single_source_shortest_path(simple_digraph, random.sample(ip,1)[0])
+            reachable_set.union(set(path.keys()))
+        print 'States that can reach Sf, size: %s' %str(len(reachable_set))
+        Sd = Sn.difference(reachable_set)
+        Sr = Sn.intersection(reachable_set)
+        # #--------------
+        print 'Sn size: %s; Sd inside size: %s; Sr inside size: %s' %(len(Sn),len(Sd), len(Sr))
+        # ---------solve lp------------
+        print '-----'
+        print 'Gurobi starts now'
+        print '-----'
+        try:
+        #if True:
+            model = Model('plan_comb')
+            #--------------------
+            #prefix variable
+            Y = defaultdict(float)
+            # create variables
+            for s in Sr:
+                for u in prod_mdp.node[s]['act'].copy():
+                    Y[(s,u)] = model.addVar(vtype=GRB.CONTINUOUS,lb=0, name='y[(%s, %s)]' %(s, u))
+            print 'Prefix Y variables added'
+            #--------------------
+            #suffix variables
+            Z = defaultdict(float)
+            for mec in S_fi:
+                sf = MEC[0]
+                ip = MEC[1]
+                act = MEC[2].copy()
+                for s in sf:
+                    for u in act[s]:
+                        Z[(s,u)] = model.addVar(vtype=GRB.CONTINUOUS,lb=0, name='z[(%s, %s)]' %(s, u))
+            print 'Suffix Z variables added'
+            model.update()
+            # set objective
+            obj = 0
+            for s in Sr:                
+                for t in prod_mdp.successors_iter(s):
+                    prop = prod_mdp.edge[s][t]['prop'].copy()
+                    for u in prop.iterkeys():
+                        pe = prop[u][0]
+                        ce = prop[u][1]
+                        obj += Y[(s,u)]*pe*ce
+            for mec in S_fi:
+                sf = MEC[0]
+                ip = MEC[1]
+                act = MEC[2].copy()
+                for s in sf:
+                    for u in act[s]:
+                        for t in prod_mdp.successors_iter(s):
+                            prop = prod_mdp.edge[s][t]['prop'].copy()
+                            if u in prop.keys():
+                                pe = prop[u][0]
+                                ce = prop[u][1]
+                                obj += alpha*Z[(s,u)]*pe*ce
+            model.setObjective(obj, GRB.MINIMIZE)
+            print 'Prefix + alpha*Suffix cost Objective function set'
+            # add constraints
+            #------------------------------
+            y_to_sd = 0.0
+            y_to_sf = 0.0
+            for s in Sr:
+                for t in prod_mdp.successors_iter(s):
+                    if t in Sd:
+                        prop = prod_mdp.edge[s][t]['prop'].copy()
+                        for u in prop.iterkeys(): 
+                            pe = prop[u][0]
+                            y_to_sd += Y[(s,u)]*pe
+                    elif t in sf:
+                        prop = prod_mdp.edge[s][t]['prop'].copy()
+                        for u in prop.iterkeys(): 
+                            pe = prop[u][0]
+                            y_to_sf += Y[(s,u)]*pe
+            model.addConstr(y_to_sf+y_to_sd >= delta, 'sum_out')
+            model.addConstr(y_to_sf >= (1.0-gamma)*(y_to_sf+y_to_sd), 'risk')            
+            print 'Prefix risk constraint added'
+            #--------------------
+            for t in Sr:
+                node_y_in = 0.0
+                node_y_out = 0.0
+                for u in prod_mdp.node[t]['act']:   
+                    node_y_out += Y[(t,u)]
+                for f in prod_mdp.predecessors_iter(t):
+                    if f in Sr:
+                        prop = prod_mdp.edge[f][t]['prop'].copy()
+                        for uf in prop.iterkeys():
+                            node_y_in += Y[(f,uf)]*prop[uf][0]
+                if t == init_node:
+                    model.addConstr(node_y_out == 1.0 + node_y_in, 'init_node_flow_balance')
+                else:
+                    model.addConstr(node_y_out == node_y_in, 'middle_node_flow_balance')
+            print 'Prefix initial node flow balanced'
+            print 'Prefix middle node flow balanced'
+            #----------------------
+            for mec in S_fi:
+                sf = MEC[0]
+                ip = MEC[1]
+                act = MEC[2].copy()
+                for s in sf:
+                    constr3 = 0
+                    constr4 = 0
+                    y_in_sf = 0
+                    for u in act[s]:
+                        constr3 += Z[(s,u)]
+                    for f in prod_mdp.predecessors_iter(s):
+                        if (f in sf) and (s not in ip):
+                            prop = prod_mdp.edge[f][s]['prop'].copy()
+                            for uf in act[f]:
+                                if uf in prop.keys():  
+                                    constr4 += Z[(f,uf)]*prop[uf][0]
+                                else:
+                                    constr4 += Z[(f,uf)]*0.00
+                        if (f in sf) and (s in ip) and (f != s):
+                            prop = prod_mdp.edge[f][s]['prop'].copy()
+                            for uf in act[f]:
+                                if uf in prop.keys():  
+                                    constr4 += Z[(f,uf)]*prop[uf][0]
+                                else:
+                                    constr4 += Z[(f,uf)]*0.00
+                        elif (f not in sf):
+                            prop = prod_mdp.edge[f][s]['prop'].copy()
+                            for uf in prop.iterkeys(): 
+                                pe = prop[uf][0]
+                                y_in_sf += Y[(f,uf)]*pe
+                    if (s not in ip):
+                        model.addConstr(constr3 == constr4 + y_in_sf, 'balance_with_y_in')
+                    elif (s in ip):
+                        model.addConstr(constr3 == y_in_sf, 'balance_with_y_in')
+                print 'Suffix balance condition added'
+                print 'Suffix initial y_in_sf condition added'            
+                #--------------------
+                y_to_ip = 0.0
+                y_out = 0.0
+                for s in sf:
+                    for t in prod_mdp.successors_iter(s):
+                        if t not in sf:
+                            prop = prod_mdp.edge[s][t]['prop'].copy()
+                            for u in prop.iterkeys():
+                                if u in act[s]:
+                                    pe = prop[u][0]
+                                    y_out += Z[(s,u)]*pe
+                        elif t in ip:
+                            prop = prod_mdp.edge[s][t]['prop'].copy()
+                            for u in prop.iterkeys():
+                                if u in act[s]:
+                                    pe = prop[u][0]
+                                    y_to_ip += Z[(s,u)]*pe
+                model.addConstr(y_to_ip+y_out >= delta, 'sum_out')
+                model.addConstr(y_to_ip >= (1.0-gamma)*(y_to_ip+y_out), 'risk')
+                print 'Suffix risk constraint added'
+            #----------------------
+            # solve
+            print '--optimization starts--'
+            model.optimize()
+            # print '--variables value--'
+            # for v in model.getVars():
+            #     print v.varName, v.x
+            # print 'obj:', model.objVal
+            #------------------------------
+            # compute plan prefix given the LP solution
+            plan_prefix = dict()
+            for s in Sr:
+                norm = 0
+                U = []
+                P = []
+                U_total = prod_mdp.node[s]['act'].copy()          
+                for u in U_total:
+                    norm += Y[(s,u)].X
+                for u in U_total:
+                    U.append(u)
+                    if norm > 0.01:
+                        P.append(Y[(s,u)].X/norm)
+                    else:
+                        P.append(1.0/len(U_total))
+                plan_prefix[s] = [U, P]
+            print "----Prefix plan generated"
+            #--------------------
+            #--------------------
+            # compute the risk given the plan prefix
+            cost_pre = 0.0
+            risk_pre = 0.0
+            y_to_sd = 0.0
+            y_to_sf = 0.0
+            for s in Sr:
+                for t in prod_mdp.successors_iter(s):
+                    prop = prod_mdp.edge[s][t]['prop'].copy()
+                    for u in prop.iterkeys():
+                        pe = prop[u][0]
+                        ce = prop[u][1]
+                        cost_pre += Y[(s,u)].X*pe*ce
+                    if t in Sd:
+                        prop = prod_mdp.edge[s][t]['prop'].copy()
+                        for u in prop.iterkeys(): 
+                            pe = prop[u][0]
+                            y_to_sd += Y[(s,u)].X*pe
+                    elif t in sf:
+                        prop = prod_mdp.edge[s][t]['prop'].copy()
+                        for u in prop.iterkeys(): 
+                            pe = prop[u][0]
+                            y_to_sf += Y[(s,u)].X*pe
+            if (y_to_sd+y_to_sf) >0:
+                risk_pre = y_to_sd/(y_to_sd+y_to_sf)
+            print 'y_to_sd: %s; y_to_sd+y_to_sf: %s' %(y_to_sd, y_to_sd+y_to_sf)
+            print "----Prefix risk computed: %s" %str(risk_pre)
+            print "----Prefix cost computed: %s" %str(cost_pre)                        
+            #--------------------
+            # compute optimal plan suffix given the LP solution
+            plan_suffix = dict()
+            for mec in S_fi:
+                sf = MEC[0]
+                ip = MEC[1]
+                act = MEC[2].copy()
+                for s in sf:
+                    norm = 0
+                    U = []
+                    P = []
+                    for u in act[s]:
+                        norm += Z[(s,u)].X
+                    for u in act[s]:
+                        U.append(u)
+                        if norm > 0.01:
+                            P.append(Z[(s,u)].X/norm)
+                        else:
+                            P.append(1.0/len(act[s])) 
+                    plan_suffix[s] = [U, P]
+            print "----Suffix plan added"
+            # compute risk given the plan suffix
+            Risk_suf = []
+            Cost_suf = []
+            for mec in S_fi:
+                risk_suf = 0.0
+                cost_suf = 0.0
+                y_to_ip = 0.0
+                y_out = 0.0
+                sf = MEC[0]
+                ip = MEC[1]
+                act = MEC[2].copy()
+                for s in sf:
+                    for t in prod_mdp.successors_iter(s):
+                        prop = prod_mdp.edge[s][t]['prop'].copy()
+                        for u in prop.iterkeys():
+                            if u in act[s]:
+                                pe = prop[u][0]
+                                ce = prop[u][1]
+                                cost_suf += alpha*Z[(s,u)].X*pe*ce
+                        if (t not in sf):
+                            prop = prod_mdp.edge[s][t]['prop'].copy()
+                            for u in prop.iterkeys():
+                                if u in act[s]:
+                                    pe = prop[u][0]
+                                    y_out += Z[(s,u)].X*pe
+                        elif (t in ip):
+                            prop = prod_mdp.edge[s][t]['prop'].copy()
+                            for u in prop.iterkeys():
+                                if u in act[s]:
+                                    pe = prop[u][0]
+                                    y_to_ip += Z[(s,u)].X*pe
+                if (y_to_ip+y_out)>0:
+                    risk_suf = y_out/(y_to_ip+y_out)
+                Risk_suf.append(risk_suf)
+                Cost_suf.append(cost_suf)
+                print 'one AMEC: y_out: %s; y_to_ip+y_out: %s' %(y_out, y_to_ip+y_out)                
+            print "----Suffix risk computed: %s" %str(Risk_suf)
+            print "----Suffix cost computed: %s" %str(Cost_suf)
+            total_cost = model.objval
+            print "----Prefix + alpha*Suffix cost computed"
+            return total_cost, plan_prefix, cost_pre, risk_pre, plan_suffix, Cost_suf, Risk_suf, [Sf,Sn,Sr,Sd]
+        except GurobiError:
+            print "Gurobi Error reported"
+            return None, None, None, None, None, None, None, None
